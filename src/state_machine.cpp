2f614fe3db2e34432ea5f7396670fad20f3fa3d7
n-shintaro <shintaro0311@keio.jp>
2021-05-16 22:31:39 +0900

    20210516 error of FSM is deleted

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1,3 +1,9 @@
+#include <memory>
+#include <chrono>
+#include <cinttypes>
+#include <memory>
+#include <cstdlib>
+
 #include "rt2_assignment1/srv/command.hpp"
 #include "rt2_assignment1/srv/position.hpp"
 #include "rt2_assignment1/srv/random_position.hpp"
@@ -6,43 +12,63 @@
 
 
 bool start = false;
-using RandomPosition = rt2_assignment1::RandomPosition;
-using Position=rt2_assignment1::Position;
-using Command=rt2_assignment1::Command;
+using RandomPosition = rt2_assignment1::srv::RandomPosition;
+using Position=rt2_assignment1::srv::Position;
+using Command=rt2_assignment1::srv::Command;
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
 
 namespace rt2_assignment1{
 class StateMachine : public rclcpp::Node
 {
     public:
+
     StateMachine(const rclcpp::NodeOptions & options)
     : Node("state_machine", options)
     {
-    service_ = this->create_service<???>("user_interface", std::bind(&StateMachine::user_interface, this, _1, _2, _3));
+    service_ = this->create_service<Command>("user_interface", std::bind(&StateMachine::user_interface, this, _1, _2, _3));
     client_rp = this->create_client<RandomPosition>("/position_server");
     client_p = this->create_client<Position>("/go_to_point");
 
-    rt2_assignment1::RandomPosition rp;
-    rp.request.x_max = 5.0;
-    rp.request.x_min = -5.0;
-    rp.request.y_max = 5.0;
-    rp.request.y_min = -5.0;
-    rt2_assignment1::Position p;
 
-    while(ros::ok()){
-   	ros::spinOnce();
-   	if (start){
-   		client_rp.call(rp);
-   		p.request.x = rp.response.x;
-   		p.request.y = rp.response.y;
-   		p.request.theta = rp.response.theta;
-   		std::cout << "\nGoing to the position: x= " << p.request.x << " y= " <<p.request.y << " theta = " <<p.request.theta << std::endl;
-   		client_p.call(p);
-   		std::cout << "Position reached" << std::endl;
-   	}
-   }
+    this->request_rp = std::make_shared<rt2_assignment1::srv::RandomPosition::Request>();
+    this->response_rp = std::make_shared<rt2_assignment1::srv::RandomPosition::Response>();
+    this->request_p = std::make_shared<rt2_assignment1::srv::Position::Request>();
+
+    this->request_rp->x_max = 5.0;
+    this->request_rp->x_min = -5.0;
+    this->request_rp->y_max = 5.0;
+    this->request_rp->y_min = -5.0;
+
+    if(start){
+        go_to_destination();
+    }
+    }
+    private:
+
+    void go_to_destination(){
+        get_random_position();
+
+        this->request_p->x = response_rp->x;
+   		this->request_p->y = response_rp->y;
+   		this->request_p->theta = response_rp->theta;
+        
+        
+
+        RCLCPP_INFO(this->get_logger(), "Go to x=%f y=%f theta=%f",
+        this->request_p->x, this->request_p->y, this->request_p->theta);
+
+        using ServiceResponseFuture =
+        rclcpp::Client<Position>::SharedFuture;
+
+        auto response_received_callback = [this](ServiceResponseFuture future) {
+        RCLCPP_INFO(this->get_logger(), "Got result: [%" PRId64 "]", future.get());
+        };
+        auto future_result = client_p->async_send_request(this->request_p, response_received_callback);
     }
 
-    private:
     void user_interface(
         const std::shared_ptr<rmw_request_id_t> request_header,
         const std::shared_ptr<Command::Request> request,
@@ -51,16 +77,42 @@ class StateMachine : public rclcpp::Node
         
     {
         (void)request_header;
-        if (req.command == "start"){
+        if (request->command == "start"){
             start = true;
         }
         else {
             start = false;
         }
-        return true;
+        response->ok=true;
     }
-    // rclcpp::Service<AddTwoInts>::SharedPtr service_;
+
+    void get_random_position(){
+        using ServiceResponseFuture =
+        rclcpp::Client<RandomPosition>::SharedFuture;
+
+        auto rp_received_callback = [this](ServiceResponseFuture future) {
+            RCLCPP_INFO(this->get_logger(), "Got result:  x=%f y=%f theta=%f", 
+            future.get()->x,future.get()->y,future.get()->theta);
+            response_rp=future.get();
+        };
+
+        auto future_result = client_rp->async_send_request(this->request_rp, rp_received_callback);
+
+    }
+
+    bool start=false;
+
+
+    rclcpp::Service<Command>::SharedPtr service_;
+    rclcpp::Client<Position>::SharedPtr client_p; 
+    rclcpp::Client<RandomPosition>::SharedPtr client_rp;
+
+    std::shared_ptr<RandomPosition::Request> request_rp;
+    std::shared_ptr<rt2_assignment1::srv::RandomPosition::Response> response_rp;
+
+    std::shared_ptr<Position::Request> request_p;
+
+    rclcpp::TimerBase::SharedPtr timer_;   
 };
 }
-
 RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::StateMachine) 
