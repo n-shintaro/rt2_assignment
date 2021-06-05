e7840bb382603c982e2e3f49c19fb6ed92afa940
shintaro <shintaro0311@keio.jp>
2021-06-06 01:30:08 +0900

    add jupyter

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -8,12 +8,11 @@
 
 bool start = false; // start becomes true when the use command start
 
+int mode=4;
 // mode=1: start
 // mode=2: reach the goal
 // mode=3 :interrupt goal
 // mode=4 : not to change
-int mode=4;
-
 
 /*
     when the user request start, mode=1.
@@ -32,32 +31,43 @@ bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Com
     return true;
 }
 
+void doneCllbck(const actionlib::SimpleClientGoalState& goal_state,
+                const rt2_assignment1::MotionResultConstPtr& result){
+  mode = 2; /* Goal reached state */
+}
+
+void activeCllbck(){return;}
+
+void feedbackCllbck(const rt2_assignment1::MotionFeedbackConstPtr& feedback){
+  //ROS_INFO("FEEDBACK: %s", feedback->status.c_str());
+  return;
+}
+
 
 int main(int argc, char **argv)
+
+
 {
    ros::init(argc, argv, "state_machine");
    ros::NodeHandle n;
-   //server
    ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
-   //client
    ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
    // action client
    actionlib::SimpleActionClient<rt2_assignment1::MotionAction> ac("/go_to_point", true);
 
-  // max and min of random position
    rt2_assignment1::RandomPosition rp;
+   // max and min of random position
    rp.request.x_max = 5.0;
    rp.request.x_min = -5.0;
    rp.request.y_max = 5.0;
    rp.request.y_min = -5.0;
-
-  while(!ac.waitForServer(ros::Duration(5.0))){
-     ROS_INFO("Please wait for action server to come up");
+    while(!ac.waitForServer(ros::Duration(5.0))){ 
+     ROS_INFO("Waiting for the go_to_point action server to come up");
    }
 
    while(ros::ok()){
    	ros::spinOnce();
-
+    // std::cout << "\n mode= " << mode<<std::endl;
    	if (mode==1){ // when user command start, send the random position as the goal to the robot
         client_rp.call(rp); // get random position
         //send the goal to the go_to_point
@@ -68,14 +78,23 @@ int main(int argc, char **argv)
         goal.actual_target.theta = rp.response.theta;
         std::cout << "\nGoing to the position: x= " << goal.actual_target.x << " y= " <<goal.actual_target.y << " theta = " <<goal.actual_target.theta << std::endl;
         ROS_INFO("Sending goal");
-        ac.sendGoal(goal); //send the goal to the robot
+        ac.sendGoal(goal, &doneCllbck, &activeCllbck, &feedbackCllbck);
         mode=4;
-
+        // else
+        //     ROS_INFO("The base failed to reach the target for some reason");
    	}
-
+    /*
+    when the user request stop, action is canceled.
+    */
+    else if(mode==3){
+        std::cout << "\n mode= " << mode<<std::endl;
+        ac.cancelGoal();
+        ROS_INFO("Goal is canceled!");
+        mode=4;
+    }
     /* action ended  when
          - the robot reach the goal
-         - the action is canceled
+         - the action is preempted
     */
     else if(mode==2){
         actionlib::SimpleClientGoalState goal_state = ac.getState();
@@ -84,21 +103,14 @@ int main(int argc, char **argv)
             mode=1; //go to the next target
         }
         else if(goal_state == actionlib::SimpleClientGoalState::PREEMPTED){
-          ROS_DEBUG("Goal is canceled");
+          ROS_DEBUG("Goal canceled");
           mode = 4;
         }
         else{
-          ROS_DEBUG("there is some error");
+          ROS_DEBUG("Fail to reach the goal");
           mode = 4;
         }
-    }
-    /*
-    when the user request stop, action is canceled.
-    */
-    else if(mode==3){
-        ac.cancelGoal(); // goal is canceled
-        ROS_INFO("Goal is canceled!");
-        mode=4;
+
     }
    }
    return 0;
